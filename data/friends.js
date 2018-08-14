480073636c47718fa9e2d3a71e75dd941facf0e3
kabrow3 <kabrow3@gmail.com>
2018-08-14 01:47:58 -0400

    partial

--- data/friends.js
+++ data/friends.js
@@ -1,6 +1,8 @@
 var friendsArray = [
     {
-        
+       name: "Kevin",
+       photo: "https://ca.slack-edge.com/T9Y37FYDU-UAC6YQ7QF-0ae894c53662-72",
+       scores: ["2", "5", "5", "1", "2","3", "5", "5", "5", "5"] 
     }
 ];
 
--- server.js
+++ server.js
@@ -13,5 +13,5 @@ require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
