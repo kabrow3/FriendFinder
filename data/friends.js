b55af67c34af6b24054a52f768b79dfc47209535
kabrow3 <kabrow3@gmail.com>
2018-08-15 01:05:00 -0400

    final?

--- data/friends.js
+++ data/friends.js
@@ -3,7 +3,28 @@ var friendsArray = [
        name: "Kevin",
        photo: "https://ca.slack-edge.com/T9Y37FYDU-UAC6YQ7QF-0ae894c53662-72",
        scores: ["2", "5", "5", "1", "2","3", "5", "5", "5", "5"] 
+    },
+    {
+        name: "Josh",
+        photo:"http://www.joshgroban.com/sites/g/files/g2000006141/f/styles/photo_detail_large/public/201610/photo-credit-brian-bowen-smith-extralarge_1462485322527_3.jpg?itok=qCNGXJ6Z",
+        scores:["5", "4", "3", "3", "2", "3", "4", "4", "4", "4"]
+    },
+    {
+        name: "Derek",
+        photo: "https://resizing.flixster.com/sCEqsSYbYRB4wzFBx8N-_GzniDo=/2395x3600/v1.bjs1MjYxODY7ajsxNzc4MTsxMjAwOzIzOTU7MzYwMA",
+        scores: ["1", "2", "4", "1", "1", "1", "3", "3", "3", "4"]
+    },
+    {
+        name: "Batman",
+        photo: "https://assets1.ignimgs.com/2018/08/13/batman-0084-1534192809903_1280w.jpg",
+        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+    {
+        name: "Humperdoo",
+        photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/7/7e/Humperdoo_Preacher_0001.jpg/revision/latest?cb=20180729022306",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
     }
+
 ];
 
 module.exports = friendsArray;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,4 +1,8 @@
 var friends = require("../data/friends");
+var express = require("express");
+
+var app = express();
+
 
 module.exports = function(app) {
     
@@ -7,8 +11,22 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
-        res.json(true);
+        var totalDifference = 0;
+        var totalTemp = 0;
+        var matchIndex;
+        var user = req.body;
+        
+        for(var i = 0; i < friends.length; i++) {
+            for (var j = 0; j <friends[i].scores.length; j++) {
+                totalTemp += Math.abs(parseInt(user.scores[j]) - parseInt(friends[i].scores[j]));
+            }
+            if (totalTemp > totalDifference) {
+                totalDifference = totalTemp;
+                matchIndex = i;
+            }
+        }
+        friends.push(user);
+        res.json(friends[matchIndex]);
     });
     
 };
